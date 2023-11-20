511fcc869d66a2c180ca945ef44105a3564e0951
PiviaN <pedro.vsnep2001@gmail.com>
2023-11-20 17:03:27 -0300

    projects

---
+++ Dice_Game/Dice_Game/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerRandomNum;
+            int enemyRandomNum;
+            int actualPlayerDice = 0;
+            int actualEnemyDice = 0;
+            int sumOfPlayerDices = 0;
+            int sumOfEnemyDices = 0;
+
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice.");
+
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine("\nYou rolled a " + playerRandomNum);
+
+                Console.WriteLine("...");
+
+                System.Threading.Thread.Sleep(1000);
+                enemyRandomNum = random.Next(1, 7);
+                Console.WriteLine("The enemy AI rolled a " + enemyRandomNum);
+
+                actualPlayerDice = playerRandomNum;
+                actualEnemyDice = enemyRandomNum;
+
+                if (actualPlayerDice > actualEnemyDice)
+                {
+                    Console.WriteLine("You won this round! \n");
+                }
+                else if (actualPlayerDice < actualEnemyDice)
+                {
+                    Console.WriteLine("You lost this round! \n");
+                } else
+                {
+                    Console.WriteLine("You have draw this round! \n");
+                }
+
+                sumOfPlayerDices += playerRandomNum;
+                sumOfEnemyDices += enemyRandomNum;
+            }
+
+            Console.WriteLine("Your total score is " + sumOfPlayerDices);
+            Console.WriteLine("The enemy AI scored " + sumOfEnemyDices);
+
+            if (sumOfPlayerDices > sumOfEnemyDices)
+            {
+                Console.WriteLine("\nCongratulations. You have won by a difference of " + (sumOfPlayerDices - sumOfEnemyDices));
+            }
+            else if (sumOfPlayerDices > sumOfEnemyDices)
+            {
+                Console.WriteLine("\nUnfortunately, you have lost by a difference of " + (sumOfEnemyDices - sumOfPlayerDices));
+            } else
+            {
+                Console.WriteLine("\nWe have a draw!");
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Quiz App/Quiz App/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+
+
+namespace Quiz_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = File.ReadAllLines("questions.txt");
+
+            List<string> questions = new List<string>();
+            List<string> answers = new List<string>();
+
+            for (int i = 0; i < text.Length; i++) { 
+                if (i % 4 == 0)
+                {
+                    questions.Add(text[i]);
+                }
+                else
+                {
+
+                }
+            }
+
+            int questionsIndex = 0;
+            int answersIndex = 0;
+            int score = 0;
+
+            while (questionsIndex < questions.Count)
+            {
+                Console.WriteLine(questions[questionsIndex]);
+                questionsIndex++;
+
+                int correctAnswerIndex = 0;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    if (answers[answersIndex].StartsWith(">"))
+                    {
+                        correctAnswerIndex = i + 1;
+                    }
+                    Console.WriteLine(i + 1 + "." + answers[answersIndex].Replace(">", ""));
+
+                    answersIndex++;
+                }
+
+                int answer = int.Parse(Console.ReadLine());
+
+                if (answer == correctAnswerIndex)
+                {
+                    Console.WriteLine("Correct!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect!");
+                }
+            }
+
+            Console.WriteLine("End of quiz! Your score was " + score);
+        }
+    }
+}
